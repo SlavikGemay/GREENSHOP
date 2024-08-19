7e9e23b0a8b90868f656074f49719610228da651
SlavikGemay <gemayit@gmail.com>
2024-08-20 00:17:54 +0300

    add dz17

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,18 @@
-// $('.owl-carousel').owlCarousel({
-//     autoplay:true,
-//     autoplayTimeout: 6500,
-//     autoplaySpeed: 1000,
-//     loop: true,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         1100:{
-//             items:4
-//         }
-//     }
-//
-// })
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout: 2000,
+    autoplaySpeed: 1000,
+    loop: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        1100:{
+            items:4
+        }
+    }
+
+})
